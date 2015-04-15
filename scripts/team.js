a62c480d0416ab141336071ae0b7015ae8eb3eb2
Kyle Shankwiler <kyle@Kyles-MacBook-Pro.local>
2015-04-14 17:20:42 -0700

    fixed indentation

--- scripts/team.js
+++ scripts/team.js
@@ -1,15 +1,15 @@
 $(document).ready(function {
   jQuery.getJSON('https://api.dvcoders.com/team', function(data) {
-  	var users = data.result;
-	
-  	for (i = 0; i < users.length; i++) {
-  		var newHtml = '<li class="col-md-4">' +
-  				'<img src="' + users[i].image_link + '">' +
-  				'<h1>' + users[i].user.fullName + '</h1>' +
-  				'<p>' + users[i].bio + '</p>' +
-  			'</li>';
-		
-  		$('.team ul').append(newHtml);
-  	}
+    var users = data.result;
+  
+    for (i = 0; i < users.length; i++) {
+      var newHtml = '<li class="col-md-4">' +
+          '<img src="' + users[i].image_link + '">' +
+          '<h1>' + users[i].user.fullName + '</h1>' +
+          '<p>' + users[i].bio + '</p>' +
+        '</li>';
+    
+      $('.team ul').append(newHtml);
+    }
   });
 });
