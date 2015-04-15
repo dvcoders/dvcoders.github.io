3175d4ad3b8afa4e9bbe103bf8b42fcc9583ebda
Kyle Shankwiler <kyle@Kyles-MacBook-Pro.local>
2015-04-14 17:18:06 -0700

    fixed link, for loop declaration, wrapped in .ready(...)

--- scripts/team.js
+++ scripts/team.js
@@ -1,13 +1,15 @@
-jQuery.getJSON('api.dvcoders.com/team', function(data) {
-	var users = data.result;
+$(document).ready(function {
+  jQuery.getJSON('https://api.dvcoders.com/team', function(data) {
+  	var users = data.result;
 	
-	for (i = 0; i < data.result.length; i++) {
-		var newHtml = '<li class="col-md-4">' +
-				'<img src="' + users[i].image_link + '">' +
-				'<h1>' + users[i].user.firstname + " " + users[i].user.lastName + '</h1>' +
-				'<p>' + users[i].bio + '</p>' +
-			'</li>';
+  	for (i = 0; i < users.length; i++) {
+  		var newHtml = '<li class="col-md-4">' +
+  				'<img src="' + users[i].image_link + '">' +
+  				'<h1>' + users[i].user.fullName + '</h1>' +
+  				'<p>' + users[i].bio + '</p>' +
+  			'</li>';
 		
-		$('.team ul').append(newHtml);
-	}
+  		$('.team ul').append(newHtml);
+  	}
+  });
 });
