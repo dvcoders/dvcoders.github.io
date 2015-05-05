015804099fe483f406aaee86eee09455047e6df4
shankwiler <kyleshankwiler@gmail.com>
2015-05-04 18:05:03 -0700

    update for new backend

--- scripts/team.js
+++ scripts/team.js
@@ -1,12 +1,12 @@
 $(document).ready(function {
   jQuery.getJSON('https://api.dvcoders.com/kernels', function(data) {
-    var results = data.result;
+    var kernels = data.result;
 
     var newHTML;
-    results.forEach( function (result) {
+    kernels.forEach( function (result) {
       newHTML = '<li class="col-md-4">' +
-          '<img src="' + result.image_link + '">' +
-          '<h1>' + result.user.fullName + '</h1>' +
+          '<img src="' + result.imageUrl + '">' +
+          '<h1>' + result.profile.fullName + '</h1>' +
           '<p>' + result.bio + '</p>' +
         '</li>';
       jQuery('.team ul').append(newHtml);
